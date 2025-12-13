95b28f3f8847d67ef741f56d5dd24b08e6b928f4
SuperWindCloud <19338823723@163.com>
2025-12-13 22:32:23 +0800

    feat: add background gradient

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,6 @@
-declare module '*.png'
-declare module '*.jpg'
+declare module "*.png";
+declare module "*.jpg";
 
-declare module '*.gif'
+declare module "*.gif";
 
-declare module '*.svg'
+declare module "*.svg";
